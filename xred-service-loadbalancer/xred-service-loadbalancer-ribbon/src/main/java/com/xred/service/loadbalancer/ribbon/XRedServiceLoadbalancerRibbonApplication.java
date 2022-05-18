bc4e89ea369c5ef4885361c4b493ac6d06f98401
zuster <zuster@sina.cn>
2022-05-18 20:17:38 +0800

    xred-service-loadbalancer | add ribbon

---
+++ xred-service-loadbalancer/xred-service-loadbalancer-ribbon/src/main/java/com/xred/service/loadbalancer/ribbon/XRedServiceLoadbalancerRibbonApplication.java
@@ -0,0 +1,13 @@
+package com.xred.service.loadbalancer.ribbon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XRedServiceLoadbalancerRibbonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XRedServiceLoadbalancerRibbonApplication.class, args);
+    }
+
+}
