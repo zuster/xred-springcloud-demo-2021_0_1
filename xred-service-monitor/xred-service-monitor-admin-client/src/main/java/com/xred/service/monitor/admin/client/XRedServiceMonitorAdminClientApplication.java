d4a1d084ea6672485938f599e44e6d688402f82b
zuster <zuster@sina.cn>
2022-05-26 22:08:21 +0800

    xred-service-monitor | add xred-service-monitor-admin-client

---
+++ xred-service-monitor/xred-service-monitor-admin-client/src/main/java/com/xred/service/monitor/admin/client/XRedServiceMonitorAdminClientApplication.java
@@ -0,0 +1,12 @@
+package com.xred.service.monitor.admin.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XRedServiceMonitorAdminClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XRedServiceMonitorAdminClientApplication.class, args);
+    }
+}
